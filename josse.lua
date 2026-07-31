e91a6a76c2fd7e2dc9527e80ac302002e3a914cb
habakkukcunger <habakkukcunger@gmail.com>
2026-07-30 22:59:57 -0400

    Update josse.lua

--- josse.lua
+++ josse.lua
@@ -1,1 +1,2 @@
+getgenv().SCRIPT_KEY = "KEYLESS"
 loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/9bc99c89695eed4133f590c7b596ad5fe8e2561c11d08846814fbc9c27748386/download"))()
